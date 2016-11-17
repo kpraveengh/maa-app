2df5a9c6e4922db98d14387be83c5d6c0c2d3504
praveen <praveen.technodrive@gmail.com>
2016-11-17 18:44:41 +0530

    chnaged allusers blade template

--- resources/views/user/allusers.blade.php
+++ resources/views/user/allusers.blade.php
@@ -17,14 +17,29 @@
     </thead>
     <tbody>
         <tr>
+        @foreach($users as $user)
         <tr>
-        <td> {{ Auth::user()->name }} </td>
-            <td>{{ $users->name }}</td>
-            <td>{{ $users->email }}</td>
-             <td>{{ $users->user_type }}</td>
+        <td>{{ $user->id }}</td>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+             <td>{{ $user->user_type }}</td>
              
-            
+             <td> {{ Form::open(array('url' => 'user/' . $user->id, 'class' => 'pull-right')) }}
+                    {{ Form::hidden('_method', 'DELETE') }}
+                    
+
+              
+                <!-- show the user (uses the show method found at GET /users/{id} -->
+                <a class="btn btn-small btn-success" href="{{ URL::to('user/' . $user->id) }}">Show</a>
+
+                <!-- edit this user (uses the edit method found at GET /users/{id}/edit -->
+                <a class="btn btn-small btn-info" href="{{ URL::to('user/' . $user->id . '/edit') }}">Edit</a>
+
+               <!-- delete the nerd (uses the destroy method DESTROY /users/{id} -->
+                    {{ Form::submit('Delete', array('class' => 'btn btn-warning')) }}
+                {{ Form::close() }}
             </tr>
+            @endforeach
            
         </tr>
     </tbody>
